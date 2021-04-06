414316fa0e38a8c50e7cb51cb6d210a7c123d1dc
David JACQUA <oshaneal@gmail.com>
2021-04-05 21:47:57 -0400

    dechiffrage - affcihage dans une autre page.

--- lib/chiffrement.dart
+++ lib/chiffrement.dart
@@ -66,7 +66,7 @@ class Chiffrement {
 
       var indexLettreInAlphabet = alphabet.indexOf(lettre);
       codeSecret = alphabetSecret[indexLettreInAlphabet];
-      
+
       messageSecret.add(codeSecret);
 
       index++;
